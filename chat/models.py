7e063ed0eb9a7bb022dd83a2bd1ecb508fe77b4f
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-26 21:34:42 +1100

    registered models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+
+class Message(models.Model):
+    million = 10 ** 6
+    value = models.CharField(max_length=million)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=million)
+    room = models.CharField(max_length=million)
