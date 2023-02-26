dd6e4be4830ff8b1f17ccf38d97ca2fc2bb7bd95
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-26 22:19:35 +1100

    Secured SECRET KEY (did not know that the first time)

--- realchat/settings.py
+++ realchat/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(f1(rexk3rczsey30sy$gjxnal72nllv+u0=8ngb8&8==a9w0$"
+# load .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
